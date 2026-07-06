ee757d138c31a9c819cb1a285dd663002aa4bed1
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-06 20:45:54 +0200

    Update api.js

--- public/dashboard/js/api.js
+++ public/dashboard/js/api.js
@@ -1,1 +1,19 @@
+export async function login(email, password){
 
+  const response = await fetch(
+    "https://api.vedlikeholdsystem.no/login",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    }
+  );
+
+  return await response.json();
+
+}
