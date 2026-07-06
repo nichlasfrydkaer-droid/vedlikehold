b9eabe5f5b3f9a8e7509d3a15458a131635b9ceb
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-06 18:18:26 +0200

    Initialize app with event listeners and job loading

--- public/motor/app.js
+++ public/motor/app.js
@@ -1,1 +1,39 @@
+import { loadJob } from "./loadJob.js";
+import { initPhotos, autoResizeNotes } from "./photos.js";
+import { initTitle } from "./title.js";
+import { generatePDF } from "./pdf.js";
+import { startWork } from "./work.js";
 import { state } from "./state.js";
+import { dom } from "./dom.js";
+
+export function initApp() {
+
+    initTitle();
+
+    initPhotos();
+
+    autoResizeNotes();
+
+    dom.startBtn.addEventListener("click", startWork);
+
+    dom.finishBtn.addEventListener("click", () => {
+
+        const navn = dom.nameInput.value.trim();
+
+        if (!navn) {
+
+            alert(state.translations.alertNameRequired);
+
+            dom.nameInput.focus();
+
+            return;
+
+        }
+
+        generatePDF();
+
+    });
+
+    loadJob();
+
+}
