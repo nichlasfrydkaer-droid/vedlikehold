d495fe1f50ea2ee76e8f64ba5dbdc3c02a4b1eec
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-06 17:06:43 +0200

    Initialize DOM elements with query selectors
    Added DOM elements using query selectors for various labels, inputs, and sections.

--- public/motor/dom.js
+++ public/motor/dom.js
@@ -1,1 +1,41 @@
+import { qs } from "./utils.js";
 
+export const dom = {
+
+  title: qs("jobTitle"),
+  subtitle: qs("jobSubtitle"),
+
+  frequency: qs("frequency"),
+
+  frequencyLabel: qs("frequencyLabel"),
+  timeLabel: qs("timeLabel"),
+  nameLabel: qs("nameLabel"),
+  checklistLabel: qs("checklistLabel"),
+  notesLabel: qs("notesLabel"),
+  photosLabel: qs("photosLabel"),
+
+  taskContainer: qs("taskContainer"),
+
+  merkText: qs("merkText"),
+  notatInfo: qs("notatInfo"),
+
+  notes: qs("notes"),
+
+  photos: qs("photos"),
+
+  preview: qs("preview"),
+
+  status: qs("status"),
+
+  startBtn: qs("startBtn"),
+  finishBtn: qs("finishBtn"),
+
+  timeInput: qs("timeInput"),
+  nameInput: qs("nameInput"),
+
+  photosSection: qs("photosSection"),
+  commentsSection: qs("commentsSection"),
+  timeSection: qs("timeSection"),
+  nameSection: qs("nameSection")
+
+};
