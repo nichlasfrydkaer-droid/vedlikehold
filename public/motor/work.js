b5aa12afdcf01141575804f80390ae201f9d3d7e
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-06 18:38:43 +0200

    Refactor startWork function for consistency

--- public/motor/work.js
+++ public/motor/work.js
@@ -2,38 +2,38 @@ import { state } from "./state.js";
 import { dom } from "./dom.js";
 import { startTimer } from "./timer.js";
 
-export function startWork() {
+export function startWork(){
 
-    startTimer();
+  startTimer();
 
-    document
-        .querySelectorAll(".task")
-        .forEach(x => x.disabled = false);
+  document
+    .querySelectorAll(".task")
+    .forEach(task => task.disabled = false);
 
-    if (
-        !state.currentCongregation ||
-        state.currentCongregation.settings.showName
-    ) {
-        dom.nameInput.disabled = false;
-    }
+  if(
+    !state.currentCongregation ||
+    state.currentCongregation.settings.showName
+  ){
+    dom.nameInput.disabled = false;
+  }
 
-    if (
-        !state.currentCongregation ||
-        state.currentCongregation.settings.allowComments
-    ) {
-        dom.notes.disabled = false;
-    }
+  if(
+    !state.currentCongregation ||
+    state.currentCongregation.settings.allowComments
+  ){
+    dom.notes.disabled = false;
+  }
 
-    if (
-        !state.currentCongregation ||
-        state.currentCongregation.settings.allowPhotos
-    ) {
-        dom.photos.disabled = false;
-    }
+  if(
+    !state.currentCongregation ||
+    state.currentCongregation.settings.allowPhotos
+  ){
+    dom.photos.disabled = false;
+  }
 
-    dom.startBtn.style.display = "none";
+  dom.startBtn.style.display = "none";
 
-    dom.finishBtn.style.display = "block";
-    dom.finishBtn.classList.add("pulse");
+  dom.finishBtn.style.display = "block";
+  dom.finishBtn.classList.add("pulse");
 
 }
