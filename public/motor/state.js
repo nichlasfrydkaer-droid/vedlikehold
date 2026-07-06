98a914c0ecc9c9afb76cb976d4df5b05ce1edcb5
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-06 16:39:01 +0200

    Initialize state object with default properties

--- public/motor/state.js
+++ public/motor/state.js
@@ -1,1 +1,30 @@
+export const state = {
 
+  // Menighed
+  congregation: null,
+
+  // Sprog
+  language: "no",
+
+  // Oversættelser
+  translations: null,
+
+  // Jobdata
+  job: null,
+
+  // Timer
+  started: false,
+  startTime: null,
+  elapsedSeconds: 0,
+
+  // PDF
+  pdfBlob: null,
+
+  // Fotos
+  photos: [],
+
+  // UI
+  sending: false,
+  finished: false
+
+};
