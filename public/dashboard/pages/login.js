cac0a579c6d9b1ab039227cc20dfbddb2821c061
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-06 20:46:13 +0200

    Implement login functionality in login.js

--- public/dashboard/pages/login.js
+++ public/dashboard/pages/login.js
@@ -1,5 +1,21 @@
+import { login } from "../js/api.js";
+import { dom } from "../js/dom.js";
+
 export function initLogin(){
 
-    console.log("Login loaded");
+  dom.loginBtn.addEventListener("click", async ()=>{
+
+    const result =
+      await login(
+        dom.email.value,
+        dom.password.value
+      );
+
+    dom.status.innerText =
+      result.message;
+
+    console.log(result);
+
+  });
 
 }
