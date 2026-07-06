7d62adff0a76410d9a73251ae0ec6ba1b2c73e5c
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-06 20:36:59 +0200

    Update dom.js

--- public/dashboard/js/dom.js
+++ public/dashboard/js/dom.js
@@ -1,1 +1,15 @@
+export const dom = {
 
+    email:
+        document.getElementById("email"),
+
+    password:
+        document.getElementById("password"),
+
+    loginBtn:
+        document.getElementById("loginBtn"),
+
+    status:
+        document.getElementById("status")
+
+};
