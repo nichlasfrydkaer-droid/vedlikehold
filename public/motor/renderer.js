9cc7fad6cc72d90912d1b63411748bb320362206
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-06 17:49:18 +0200

    Implement renderTasks function in renderer.js

--- public/motor/renderer.js
+++ public/motor/renderer.js
@@ -1,1 +1,12 @@
 import { state } from "./state.js";
+
+export function renderTasks() {
+  const container = document.getElementById("taskContainer");
+  container.innerHTML = "";
+
+  state.currentJob.oppgaver.forEach(task => {
+
+    // ... hele den store forEach-blok ...
+
+  });
+}
