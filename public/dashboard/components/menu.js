50a528952541a6b4b97401406f7c16f6cd6f02da
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-07 20:54:57 +0200

    Add renderMenu function to create app menu

--- public/dashboard/components/menu.js
+++ public/dashboard/components/menu.js
@@ -1,1 +1,58 @@
+export function renderMenu(){
 
+    return `
+
+        <div
+            id="appMenu"
+            class="app-menu hidden"
+        >
+
+            <button>
+
+                📋
+                Jobbkort
+
+            </button>
+
+            <button>
+
+                📄
+                Rapporter
+
+            </button>
+
+            <button>
+
+                🔧
+                Oppdrag
+
+            </button>
+
+            <button>
+
+                👥
+                Brukere
+
+            </button>
+
+            <button>
+
+                ⚙️
+                Innstillinger
+
+            </button>
+
+            <hr>
+
+            <button>
+
+                🚪
+                Logg ut
+
+            </button>
+
+        </div>
+
+    `;
+
+}
