3c9204b0176b63adeb2ed3a1e66bf180234aa087
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-07 21:13:32 +0200

    Add dashboard grid structure to initDashboard
    Initialize dashboard structure with grid layout.

--- public/dashboard/pages/dashboard.js
+++ public/dashboard/pages/dashboard.js
@@ -1,6 +1,7 @@
 import { renderDashboardHeader } from "../components/dashboardHeader.js";
 import { renderDashboardMenu } from "../components/dashboardMenu.js";
 import { initDashboardMenu } from "../components/dashboardMenu.js";
+
 import { renderDashboardNews } from "../components/dashboardNews.js";
 import { renderDashboardOverview } from "../components/dashboardOverview.js";
 import { renderDashboardUpcoming } from "../components/dashboardUpcoming.js";
@@ -10,11 +11,25 @@ import { renderDashboardFooter } from "../components/dashboardFooter.js";
 
 export async function initDashboard() {
 
+    const dashboard =
+        document.getElementById("dashboard");
+
+    dashboard.innerHTML = "";
+
     renderDashboardHeader();
 
     renderDashboardMenu();
 
     initDashboardMenu();
+
+    dashboard.insertAdjacentHTML(
+        "beforeend",
+        `
+        <div id="dashboardGrid"
+             class="dashboard-grid">
+        </div>
+        `
+    );
 
     renderDashboardNews();
 
