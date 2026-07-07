9a3f4ad48d9873e0618a415dc7a4186fc8981ef5
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-07 21:41:56 +0200

    Fetch user data and update state in dashboard

--- public/dashboard/pages/dashboard.js
+++ public/dashboard/pages/dashboard.js
@@ -1,3 +1,6 @@
+import { getMe } from "../js/api.js";
+import { state } from "../js/state.js";
+
 import { renderDashboardHeader } from "../components/dashboardHeader.js";
 import { renderDashboardMenu } from "../components/dashboardMenu.js";
 import { initDashboardMenu } from "../components/dashboardMenu.js";
@@ -16,20 +19,71 @@ export async function initDashboard() {
 
     dashboard.innerHTML = "";
 
+    //
+    // Hent den aktuelle bruger
+    //
+
+    const me =
+        await getMe();
+
+    if(me.success){
+
+        state.user =
+            me.user;
+
+        state.congregations =
+            me.congregations;
+
+        if(me.congregations.length){
+
+            state.congregation =
+                me.congregations[0];
+
+        }
+
+    }else{
+
+        localStorage.removeItem(
+            "dashboard_token"
+        );
+
+        window.location.href =
+            "/dashboard/login.html";
+
+        return;
+
+    }
+
+    //
+    // Header
+    //
+
     renderDashboardHeader();
 
     renderDashboardMenu();
 
     initDashboardMenu();
 
+    //
+    // Dashboard Grid
+    //
+
     dashboard.insertAdjacentHTML(
+
         "beforeend",
+
         `
-        <div id="dashboardGrid"
-             class="dashboard-grid">
+        <div
+            id="dashboardGrid"
+            class="dashboard-grid">
         </div>
         `
+
     );
+
+    //
+    // Widgets
+    //
 
     renderDashboardNews();
 
