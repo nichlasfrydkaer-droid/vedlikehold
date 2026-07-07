67b5b1111c7db98d95d3403442fa0a9d0166b46e
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-07 21:05:13 +0200

    Add dashboard menu rendering and initialization

--- public/dashboard/pages/dashboard.js
+++ public/dashboard/pages/dashboard.js
@@ -1,4 +1,6 @@
 import { renderDashboardHeader } from "../components/dashboardHeader.js";
+import { renderDashboardMenu } from "../components/dashboardMenu.js";
+import { initDashboardMenu } from "../components/dashboardMenu.js";
 import { renderDashboardNews } from "../components/dashboardNews.js";
 import { renderDashboardOverview } from "../components/dashboardOverview.js";
 import { renderDashboardUpcoming } from "../components/dashboardUpcoming.js";
@@ -9,6 +11,10 @@ import { renderDashboardFooter } from "../components/dashboardFooter.js";
 export async function initDashboard() {
 
     renderDashboardHeader();
+
+    renderDashboardMenu();
+
+    initDashboardMenu();
 
     renderDashboardNews();
 
