0d8e1c77f7f7968283b0abfa4c4ce48eeee7176d
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-07 21:06:40 +0200

    Add createCard function to generate dashboard cards

---
+++ public/dashboard/components/card.js
@@ -0,0 +1,19 @@
+export function createCard(title, content) {
+
+    return `
+
+        <section class="dashboard-card">
+
+            <h2>
+
+                ${title}
+
+            </h2>
+
+            ${content}
+
+        </section>
+
+    `;
+
+}
