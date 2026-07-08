62f0df3a9841bca828e7fb7c369fa872e384135c
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-08 19:54:29 +0200

    Add section function to render report sections

--- public/dashboard/ui/section.js
+++ public/dashboard/ui/section.js
@@ -1,1 +1,22 @@
+export function section(
+    title,
+    content
+){
 
+    return `
+
+        <div class="report-section">
+
+            <h3>
+
+                ${title}
+
+            </h3>
+
+            ${content}
+
+        </div>
+
+    `;
+
+}
