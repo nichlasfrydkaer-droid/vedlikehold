c07e5089db52c1be46c44d08e06d0c61e67a0b6e
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-08 20:07:45 +0200

    Add loadReport function to fetch report data

--- public/dashboard/services/reports.js
+++ public/dashboard/services/reports.js
@@ -1,1 +1,16 @@
+import { getReport } from "../js/api.js";
 
+export async function loadReport(id){
+
+    const result =
+        await getReport(id);
+
+    if(!result.success){
+
+        return null;
+
+    }
+
+    return result;
+
+}
