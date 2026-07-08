0ae57d8e8fe2eabb2ae5a02fd3231bf1a07f8c11
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-08 20:15:55 +0200

    Refactor task service to include getTask function

--- public/dashboard/services/tasks.js
+++ public/dashboard/services/tasks.js
@@ -1,7 +1,35 @@
-import { createTask } from "../js/api.js";
+import {
 
-export async function saveTask(task){
+    createTask,
 
-    return await createTask(task);
+    getTask
+
+}
+from "../js/api.js";
+
+export async function loadTask(
+    id
+){
+
+    const result =
+        await getTask(id);
+
+    if(!result.success){
+
+        return null;
+
+    }
+
+    return result.task;
+
+}
+
+export async function saveTask(
+    task
+){
+
+    return await createTask(
+        task
+    );
 
 }
