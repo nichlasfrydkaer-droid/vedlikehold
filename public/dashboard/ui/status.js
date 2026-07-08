60f96156301ad1f07ad75c02d2700de19aaacf52
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-08 19:54:51 +0200

    Update status.js

--- public/dashboard/ui/status.js
+++ public/dashboard/ui/status.js
@@ -1,1 +1,43 @@
+export function status(status){
 
+    let icon = "⚪";
+    let text = status;
+
+    switch(status){
+
+        case "completed":
+
+            icon = "🟢";
+            text = "Fullført";
+
+            break;
+
+        case "open":
+
+            icon = "🟠";
+            text = "Åpent";
+
+            break;
+
+        case "created":
+
+            icon = "🟡";
+            text = "Opprettet";
+
+            break;
+
+    }
+
+    return `
+
+        <div class="status">
+
+            ${icon}
+
+            ${text}
+
+        </div>
+
+    `;
+
+}
