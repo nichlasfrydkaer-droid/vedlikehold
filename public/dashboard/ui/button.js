69bbf8e8741b5af9a19f6bf9f3b1f66df1233fe4
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-08 19:54:42 +0200

    Add button function to create button elements

--- public/dashboard/ui/button.js
+++ public/dashboard/ui/button.js
@@ -1,1 +1,31 @@
+export function button({
 
+    id = "",
+
+    text,
+
+    className = "button-primary",
+
+    icon = ""
+
+}){
+
+    return `
+
+        <button
+
+            id="${id}"
+
+            class="${className}"
+
+        >
+
+            ${icon}
+
+            ${text}
+
+        </button>
+
+    `;
+
+}
