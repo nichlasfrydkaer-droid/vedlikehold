3e3c71bc68f564f9f010de4faf75870478c31a6e
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-08 21:37:20 +0200

    Refactor dashboard.js for improved structure

--- public/dashboard/pages/dashboard.js
+++ public/dashboard/pages/dashboard.js
@@ -3,57 +3,57 @@ import {
 }
 from "../services/dashboard.js";
 
-const me =
-    await loadDashboard();
-
-if(!me){
-
-    return;
-
-}
-import { getMe } from "../js/api.js";
-import { state } from "../js/state.js";
-import {
-    loadCongregation
-} from "../js/session.js";
-import {
-    loadTranslations
-} from "../js/i18n.js";
-
 import {
     renderDashboardHeader
-} from "../components/dashboardHeader.js";
+}
+from "../components/dashboardHeader.js";
 
 import {
     renderDashboardMenu,
     initDashboardMenu
-} from "../components/dashboardMenu.js";
+}
+from "../components/dashboardMenu.js";
 
 import {
     renderDashboardNews
-} from "../components/dashboardNews.js";
+}
+from "../components/dashboardNews.js";
 
 import {
     renderDashboardOverview
-} from "../components/dashboardOverview.js";
+}
+from "../components/dashboardOverview.js";
 
 import {
     renderDashboardUpcoming
-} from "../components/dashboardUpcoming.js";
+}
+from "../components/dashboardUpcoming.js";
 
 import {
     renderDashboardTasks
-} from "../components/dashboardTasks.js";
+}
+from "../components/dashboardTasks.js";
 
 import {
     renderDashboardReports
-} from "../components/dashboardReports.js";
+}
+from "../components/dashboardReports.js";
 
 import {
     renderDashboardFooter
-} from "../components/dashboardFooter.js";
+}
+from "../components/dashboardFooter.js";
 
 export async function initDashboard(){
+
+    const me =
+        await loadDashboard();
+
+    if(!me){
+
+        return;
+
+    }
 
     const dashboard =
         document.getElementById(
@@ -61,49 +61,6 @@ export async function initDashboard(){
         );
 
     dashboard.innerHTML = "";
-
-    const me =
-        await getMe();
-
-    if(!me.success){
-
-        localStorage.removeItem(
-            "dashboard_token"
-        );
-
-        localStorage.removeItem(
-            "dashboard_congregation"
-        );
-
-        location.href =
-            "/dashboard/login.html";
-
-        return;
-
-    }
-
-    state.user =
-        me.user;
-
-    state.congregations =
-        me.congregations;
-
-    if(me.congregations.length){
-
-        state.congregation =
-            me.congregations[0];
-
-    }
-
-    loadCongregation();
-
-    await loadTranslations(
-
-        state.congregation?.language ??
-
-        "no"
-
-    );
 
     renderDashboardHeader();
 
