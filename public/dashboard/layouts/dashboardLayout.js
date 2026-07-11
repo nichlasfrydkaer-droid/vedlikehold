e4be6f014fa6632345ed98e9a8cd215c4eded6e7
nichlasfrydkaer-droid <nichlasfrydkaer@gmail.com>
2026-07-11 21:17:56 +0200

    logo

--- public/dashboard/layouts/dashboardLayout.js
+++ public/dashboard/layouts/dashboardLayout.js
@@ -14,6 +14,7 @@ import {
 const dashboardStyles = [
     "/dashboard/css/dashboardHeader.css",
     "/dashboard/css/dashboardMenu.css",
+    "/dashboard/css/dashboardSidebarFixes.css",
     "/dashboard/css/dashboardCongregationSelector.css"
 ];
 
